79eaa7b5acf1e24a1cfa4b2b8ea2a8daac80709b
Rashford <mikearowo@gmail.com>
2023-02-19 23:08:37 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/main.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+    return (
+        <div>
+            This is the home page
+        </div>
+    )
+}
