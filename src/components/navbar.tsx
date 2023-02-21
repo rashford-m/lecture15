cf12408cd272009e09abf83d72e9539e4fd5080a
Rashford <mikearowo@gmail.com>
2023-02-21 22:39:05 +0100

    updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
+      <div>
+        <p>{auth.currentUser?.displayName}</p>
+        <img
+          src={auth.currentUser?.photoURL || ""}
+          width="100"
+          height="100"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
